46e91d6981e797df06a851080256e9ae0fc7a2b1
Florian Meier <florian-meier@posteo.de>
2022-12-13 22:35:24 +0100

    Day01 Part 2 Solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -11,13 +10,19 @@ fn main() {
 
     let splitted_data = data.lines();
 
-    let mut old: u32 = 0;
+    // let mut old: u32 = 0;
+    let mut top_three: [u32; 3] = [0, 0, 0];
+
     let mut buffer: u32 = 0;
 
     for line in splitted_data {
         if line == "" {
-            if old < buffer {
-                old = buffer;
+            for it in &mut top_three.iter_mut() {
+                if it < &mut buffer {
+                    let tmp: u32 = buffer;
+                    buffer = *it;
+                    *it = tmp;
+                }
             }
             buffer = 0;
         } else {
@@ -27,8 +32,11 @@ fn main() {
     }
 
     // Output greatest calories bag
-    print!(
-        "The elv with the food containing the most calories has in total {} calories!",
-        old
+    println!(
+        "The elv with the food containing the most calories has in total {:?} calories!",
+        top_three
     );
+
+    let result: u32 = top_three[0] + top_three[1] + top_three[2];
+    println!("In total they carry {} calories!", result)
 }
